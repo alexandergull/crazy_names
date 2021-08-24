9c8204e717d897048107147a3c759cf35acd9d3b
Cadence <r2e5@ya.ru>
2021-08-24 22:14:47 +0500

    IMP: Movement part of defs from main calss to Word class NEW: WORD stats report NEW: WORD length multiplicator researh

--- main.py
+++ main.py
@@ -6,11 +6,72 @@ import re
 
 
 class Word:
-    def __init__(self, word='', twins_list=None, trines_list=None, lexems_amount=0):
+    def __init__(self, word=''):
         self.word = word
-        self.twins_list = twins_list
-        self.trines_list = trines_list
-        self.lexems_amount = lexems_amount
+        self.twins_list = []
+        self.trines_list = []
+        self.trines_amount = 0
+        self.twins_amount = 0
+        self.lexems_amount = 0
+        self.twins_detected_dict = {}
+        self.trines_detected_dict = {}
+
+    def calculate_lexems(self, lang_twins_dict, lang_trines_dict):
+
+        for twin in self.twins_list:
+            if twin in lang_twins_dict.keys():
+                self.twins_amount += lang_twins_dict[twin]
+                self.twins_detected_dict[twin] = lang_twins_dict[twin]
+
+        for trine in self.trines_list:
+            if trine in lang_trines_dict.keys():
+                self.trines_amount += lang_trines_dict[trine]
+                self.twins_detected_dict[trine] = lang_trines_dict[trine]
+
+    def collect_lexems(self, strafe):
+        twins_iterator = 1
+        trines_iterator = 1
+        current_twin = ''
+        current_trine = ''
+        for char in self.word[strafe:len(self.word)]:
+
+            if trines_iterator < 3:
+                current_trine += char
+                trines_iterator += 1
+            else:
+                current_trine += char
+                self.trines_list.append(current_trine)
+                trines_iterator = 2
+                current_trine = char
+
+            if strafe < 2:
+                if twins_iterator < 2:
+                    current_twin += char
+                    twins_iterator += 1
+                else:
+                    current_twin += char
+                    self.twins_list.append(current_twin)
+                    current_twin = char
+                    twins_iterator = 2
+
+    def check_word(self, lang_twins_dict, lang_trines_dict, twin_multiplicator, trine_multiplicator):
+
+        if self.word:
+
+            self.collect_lexems(0)
+            self.collect_lexems(1)
+            self.collect_lexems(2)
+
+            self.calculate_lexems(lang_twins_dict, lang_trines_dict)
+
+            x = len(self.word)
+
+            power = -0.1 * x
+
+            word_length_multiplier = 12 * pow(x, power)
+
+            self.lexems_amount = word_length_multiplier * ((self.twins_amount * twin_multiplicator) + (
+                        self.trines_amount * trine_multiplicator))
 
 
 class CrazyName:
@@ -80,31 +141,11 @@ class CrazyName:
 
                 # print(f'TRINES LIST {self.trines_list}')
 
-    def collect_word_lexems(self, word, strafe):
-        twins_iterator = 1
-        trines_iterator = 1
-        current_twin = ''
-        current_trine = ''
-        for char in word[strafe:len(word)]:
+    def normalize_dicts(self, normal):
+        for twin in self.twins_dict:
+            if self.twins_dict[twin] > normal:
+                self.twins_dict[twin] = self.twins_dict[twin] // 3
 
-            if trines_iterator < 3:
-                current_trine += char
-                trines_iterator += 1
-            else:
-                current_trine += char
-                self.word_trines_list.append(current_trine)
-                trines_iterator = 2
-                current_trine = char
-
-            if strafe < 2:
-                if twins_iterator < 2:
-                    current_twin += char
-                    twins_iterator += 1
-                else:
-                    current_twin += char
-                    self.word_twins_list.append(current_twin)
-                    current_twin = char
-                    twins_iterator = 2
 
     def calculate_dicts_in_dictionary(self):
         for twin in self.twins_list:
@@ -117,16 +158,6 @@ class CrazyName:
                 self.trines_dict[trine] = self.trines_list.count(trine)
         # print(self.trines_dict)
 
-    def calculate_lexems_found_in_word(self):
-
-        for twin in self.word_twins_list:
-            if twin in self.twins_dict.keys():
-                self.word_twins_amount += self.twins_dict[twin]
-
-        for trine in self.word_trines_list:
-            if trine in self.trines_dict.keys():
-                self.word_trines_amount += self.trines_dict[trine]
-
     def init_data(self):
 
         self.init_dictionary_file()
@@ -137,9 +168,10 @@ class CrazyName:
 
         self.calculate_dicts_in_dictionary()
 
+        self.normalize_dicts(100)
+
     def check_word_list_from_file(self, filepath):
-        file = open(filepath, 'r')
-        clear_list = []
+        file = open(filepath, 'r', encoding='UTF-8')
         word_list = []
         while True:
             line = file.readline()
@@ -149,66 +181,46 @@ class CrazyName:
         file.close()
 
         for word in word_list:
-            result = word.replace('\n', '')
-            if result != '': clear_list.append(result)
+            if not word.count('http') and not word.count('.'):
+                result = word.replace('\n', '').replace('-', '').replace('_', '')
+                if result != '' and result not in self.word_input_list:
+                    self.word_input_list.append(Word(result))
 
-        print(f'WORDS COUNT:{len(clear_list)}')
+        print(f'WORDS COUNT:{len(self.word_input_list)}')
 
-        for word in clear_list:
-            self.check_word(word)
+        for word in self.word_input_list:
+            word.check_word(self.twins_dict, self.trines_dict, self.twin_multiplicator, self.trine_multiplicator)
 
-    def check_word(self, word):
-
-        if word:
-
-            self.word_twins_list = []
-            self.word_trines_list = []
-            self.word_twins_amount = 0
-            self.word_trines_amount = 0
-
-            self.collect_word_lexems(word, 0)
-            self.collect_word_lexems(word, 1)
-            self.collect_word_lexems(word, 2)
-
-            self.calculate_lexems_found_in_word()
-
-            word_length_multiplier = len(word) * pow(len(word),-1)
-
-            if len(word) == 1:
-                word_length_multiplier = 100
-            elif len(word) == 2:
-                word_length_multiplier = 60
-            elif len(word) == 3:
-                word_length_multiplier = 50
-            elif len(word) == 4:
-                word_length_multiplier = 3
-            elif len(word) == 5:
-                word_length_multiplier = 2
-
-
-            print(self.word_twins_amount, self.word_trines_amount)
-
-            total_amount = (self.word_twins_amount * self.twin_multiplicator * word_length_multiplier) + \
-                           (self.word_trines_amount * self.trine_multiplicator * word_length_multiplier)
-
-            self.word_input_list.append(
-                Word(word, self.word_twins_list, self.word_trines_list, lexems_amount=total_amount))
-
-    def report_words_stat(self):
+    def report_words_stat(self, show_odds_only=1, amount_gain_to_show=100):
         odd_words_total = 0
         good_words_total = 0
 
         for word in self.word_input_list:
+
             if word.lexems_amount < self.sensitivity:
-                print(f'WORD: {word.word}')
-                print(f'WORD TWINS: {word.twins_list}')
-                print(f'WORD TRINES: {word.trines_list}')
-                print(f'WORD AMOUNT: {word.lexems_amount}')
-                print('WORD IS ODD')
-            if word.lexems_amount >= self.sensitivity:
-                good_words_total += 1
-            else:
                 odd_words_total += 1
+            else:
+                good_words_total += 1
+
+            if word.lexems_amount > self.sensitivity:
+                if show_odds_only != 1:
+                    print(f'WORD: {word.word}')
+                    print(f'TWINS: {word.twins_list}: TTL {word.twins_amount}')
+                    print(f'DETECTED TWINS DICT: {word.twins_detected_dict}')
+                    print(f'TRINES: {word.trines_list}: TTL {word.trines_amount}')
+                    print(f'DETECTED TRINES DICT: {word.trines_detected_dict}')
+                    print(f'AMOUNT: {word.lexems_amount}')
+                    print('WORD IS FINE\n')
+            else:
+                if word.lexems_amount > amount_gain_to_show:
+                    print(f'WORD: {word.word}')
+                    print(f'TWINS: {word.twins_list}: TTL {word.twins_amount}')
+                    print(f'DETECTED TWINS DICT: {word.twins_detected_dict}')
+                    print(f'TRINES: {word.trines_list}: TTL {word.trines_amount}')
+                    print(f'DETECTED TRINES DICT: {word.trines_detected_dict}')
+                    print(f'AMOUNT: {word.lexems_amount}')
+                    print('WORD IS ODD\n')
+
         words_total = good_words_total + odd_words_total
         percentage = odd_words_total / (words_total / 100)
         print(f'PERCENTAGE OF ODD WORDS {percentage}')
@@ -216,9 +228,9 @@ class CrazyName:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    c_name = CrazyName(dict_filepath='utf2.txt', sensitivity=1000, trine_multiplicator=33, twin_multiplicator=1)
-    c_name.init_data()
-    c_name.check_word_list_from_file('utf2.txt')
-    c_name.report_words_stat()
+    crazy = CrazyName(dict_filepath='utf.txt', sensitivity=200, trine_multiplicator=20, twin_multiplicator=1)
+    crazy.init_data()
+    crazy.check_word_list_from_file('utf.txt')
+    crazy.report_words_stat(show_odds_only=1, amount_gain_to_show=25)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
